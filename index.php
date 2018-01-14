86226f15577ac9b59bf5fd222984da5fcffc5b8c
zver151190 <15kain@gmail.com>
2018-01-14 21:33:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -67,7 +67,171 @@
 				<div class="keyart_layer parallax" id="keyart-7" data-speed="69"></div>		<!-- 87.5 -->
 				<div class="keyart_layer" id="keyart-8" data-speed="100"></div>		<!-- 100. -->
 </div>
+  <!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Artiom Bublik</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+</head>
+<style>
+#keyart-0 {
+    background-image: url(http://www.firewatchgame.com/images/parallax/parallax0.png);
+    background-color: #ffaf1b;
+    background-position-y: -100px;
+}
+#keyart-1 {
+    background-image: url(http://www.firewatchgame.com/images/parallax/parallax1.png);
+    background-position-y: -100px;
+}
+#keyart-2 {
+    background-image: url(https://sortexpress.com/images/parallax_2.png);
+    background-position-y: -100px;
+}
+#keyart-3 {
+    background-image: url(http://www.firewatchgame.com/images/parallax/parallax3.png);
+    background-position-y: -100px;
+}
+#keyart-4 {
+    background-image: url(http://www.firewatchgame.com/images/parallax/parallax4.png);
+    background-position-y: -100px;
+}
+#keyart-5 {
+    background-image: url(http://www.firewatchgame.com/images/parallax/parallax5.png);
+    background-position-y: -100px;
+}
+#keyart-6 {
+    background-image: url(http://www.firewatchgame.com/images/parallax/parallax6.png);
+    background-position-y: -100px;
+}
+#keyart-7 {
+    background-image: url(https://sortexpress.com/images/parallax_7a.png);
+    background-position-y: 0px;
+    background-size: auto 1838px;
+}
+#keyart-8 {
+    background-image: url(https://sortexpress.com/images/parallax_8.png);
+    background-position-y: -100px;
+    background-size: auto 1838px;
+}
+.keyart_layer {
+    background-position: bottom center;
+    background-size: auto 1038px;
+    background-repeat: repeat-x;
+    width: 100%;
+    position: absolute;
+}
+.keyart, .keyart_layer {
+    height: 1200px;
+}
+ .keyart_layer.parallax {
+    position: fixed;
+	transform:translate3d(0px,0px,0px);
+	transition:transform 0.3s ease;
+}
+</style>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.js"  integrity="sha256-tA8y0XqiwnpwmOIl3SGAcFl2RvxHjA8qp0+1uCGmRmg=" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.js" crossorigin="anonymous"></script>
+  <script src="https://fatlinesofcode.github.io/jquery.smoothwheel/src/jquery.smoothwheel.js" crossorigin="anonymous"></script>
+  <script>
+
+
+
+		$(document).ready(function () {
+
+			var lastScrollTop = 0;
+			var distance = 0;
+			$(window).on('mousewheel', function(event) {
+					var st = $(this).scrollTop();
+					if( $(this).scrollTop() < 800){
+						if (event.deltaY == -1){
+							var layers = $('.parallax');
+							$.each(layers,function(index,value){
+								$(value).attr("clicks",parseInt($(value).attr("clicks"))+1);
+								var matrix = $(value).css('transform').replace(/[^0-9\-.,]/g, '').split(',');
+								var y = parseInt($(value).attr('clicks'));
+								var multiplier = parseInt($(value).attr('data-speed'));
+								var result = ((parseInt(multiplier)*parseInt(y)));
+								$(value).css("transform","translate3d(0px,-"+result+"px,0px)");
+								
+								
+							});
+						} else if( $(this).scrollTop() > 0 && event.deltaY == 1 && $(this).scrollTop() < 1000){
+
+							var layers = $('.parallax');
+							$.each(layers,function(index,value){
+								if( parseInt($(value).attr("clicks")) <= 0){
+									if($(this).scrollTop()==0){
+										$(value).attr("clicks","0");
+									}
+								}else{
+									$(value).attr("clicks",parseInt($(value).attr("clicks"))-1);
+								}
+								var matrix = $(value).css('transform').replace(/[^0-9\-.,]/g, '').split(',');
+								var y = parseInt($(value).attr('clicks'));
+								var multiplier = parseInt($(value).attr('data-speed')); 
+								var result = ((parseInt(multiplier)*parseInt(y)));
+								
+								$(value).css("transform","translate3d(0px,-"+result+"px,0px)");
+								
+							});
+						}
+					}else if( $(this).scrollTop() >= 800 ){
+						var layers = $('.parallax');
+							$.each(layers,function(index,value){
+								$(value).attr("clicks","4");
+								var y = parseInt($(value).attr('clicks'));
+								var multiplier = parseInt($(value).attr('data-speed')); 
+								var result = ((parseInt(multiplier)*parseInt(y)));
+								$(value).css("transform","translate3d(0px,-"+result+"px,0px)");
+							});
+										
+					}
+					distance+=event.deltaY;
+					if( $(this).scrollTop() == 0){
+						distance = 0;
+					}
+					console.log(distance);
+			});
+
+              $(window).scroll(function(){
+				  var layers = $('.parallax');
+				  if( $(this).scrollTop() == 0 ){
+					  $.each(layers,function(index,value){
+						$(value).attr("clicks","0");
+						$(value).css("transform","translate3d(0px,0px,0px)");
+					  });
+				  }
+			  });
+		});
+
+	
+</script>
+<body>
+
+
+  <div class="keyart" id="parallax">
+				<div class="keyart_layer parallax" id="keyart-0" data-speed="2" clicks="0" style="transform: translate3d(0px, 0px, 0px);"></div>		<!-- 00.0 -->
+				<div class="keyart_layer parallax" id="keyart-1" data-speed="5" clicks="0" style="transform: translate3d(0px, 0px, 0px);"></div>	<!-- 12.5 -->
+				<div class="keyart_layer parallax" id="keyart-2" data-speed="11" clicks="0" style="transform: translate3d(0px, 0px, 0px);"></div>		<!-- 25.0 -->
+				<div class="keyart_layer parallax" id="keyart-3" data-speed="16" clicks="0" style="transform: translate3d(0px, 0px, 0px);"></div>	<!-- 37.5 -->
+				<div class="keyart_layer parallax" id="keyart-4" data-speed="26" clicks="0" style="transform: translate3d(0px, 0px, 0px);"></div>		<!-- 50.0 -->
+				<div class="keyart_layer parallax" id="keyart-5" data-speed="36" clicks="0" style="transform: translate3d(0px, 0px, 0px);"></div>	<!-- 62.5 -->
+				<div class="keyart_layer parallax" id="keyart-6" data-speed="49" clicks="0" style="transform: translate3d(0px, 0px, 0px);"></div>		<!-- 75.0 -->
+				<div class="keyart_layer" id="keyart-scrim"></div>
+				<div class="keyart_layer parallax" id="keyart-7" data-speed="69" clicks="0" style="transform: translate3d(0px, 0px, 0px);"></div>		<!-- 87.5 -->
+				<div class="keyart_layer " id="keyart-8" data-speed="100"></div>		<!-- 100. -->
+</div>
   
+<div style="height:1000px;width:  100%;background: #210002;z-index: 10;position: relative;margin-bottom: -270px;"></div>
+  
+
+</body>
+
+</html>
   
   </body>
 
