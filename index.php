7fdf0c46d0437d493343f68c0aae31ef63768d57
zver151190 <15kain@gmail.com>
2018-01-15 15:49:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -103,7 +103,6 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.js" crossorigin="anonymous"></script>
   <script src="https://fatlinesofcode.github.io/jquery.smoothwheel/src/jquery.smoothwheel.js" crossorigin="anonymous"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
 	
 	<script>
 
