8d16668784f10a6fdcff0b8d17b2ba9a7b39ca10
zver151190 <15kain@gmail.com>
2018-01-15 14:56:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -161,19 +161,19 @@
 @media screen and (max-width: 1800px) {
 	#q .col-md-3 h3 {min-height: 90px !important;}
 }
-@media screen and (max-width: 800px) {
-	#q .col-md-3{width:100% !important;}
-	#q .col-md-3 p{font-size:22px !important;}
-	#_about_me{height:2000px;}
-}
 	
 @media screen and (max-width: 1550px){
-	#q .col-md-3{width:50% !important}
+	#q .col-md-3{width:50%;}
 	#_about_me{height:1400px;}
 }
 @media screen and (max-width: 990px) {
 	#_about-cont .col-md-4 div{margin-bottom:90px;}
 	#_about_me {height: 2000px;}
+}
+@media screen and (max-width: 800px) {
+	#q .col-md-3{width:100%;}
+	#q .col-md-3 p{font-size:22px;}
+	#_about_me{height:2000px;}
 }
 
 </style>
